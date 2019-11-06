85c398278818e0acf665fd32782b1e594aef6a95
olanetsoft <heedris2olubisi@gmail.com>
2019-11-06 21:48:54 +0100

    Venue Nfo Updated and completed

--- src/components/VenueNfo/Index.js
+++ src/components/VenueNfo/Index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Zoom from 'react-reveal/Zoom';
 
 //import icon_calender from '../../resources/images/icons/calender.png';
 import icon_location from '../../resources/images/icons/location.png';
@@ -7,26 +8,64 @@ import icon_location from '../../resources/images/icons/location.png';
 
 const VenueNfo = () => {
     return (
+        //This section below creates the nice looking section
         <div className="bck_black"> 
            <div className="center_wrapper">
                 <div className="vn_wrapper">
 
-                    <div className="vn_item">
-                        <div className="vn_outer">
-                            <div className="vn_inner">
-                                <div className="vn_icon_square bck_red"></div>
-                                <div
-                                    className="vn_icon"
-                                    style={{
-                                        background: `url(${icon_location})`
-                                    }}
-                                >
+                    <Zoom duration={500}>
 
+                        <div className="vn_item">
+                            <div className="vn_outer">
+                                <div className="vn_inner">
+                                    <div className="vn_icon_square bck_red"></div>
+                                    <div
+                                        className="vn_icon"
+                                        style={{
+                                            background: `url(${icon_location})`
+                                        }}
+                                    >
+
+                                    </div>
+                                    <div className="vn_title">
+                                            Event Date & Time
+                                    </div>
+                                    <div className="vn_desc">
+                                        01 January 2020 @10.00 am
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
+                    </Zoom>
+
+
+                    <Zoom duration={500} delay={500}>
+
+                        <div className="vn_item">
+                            <div className="vn_outer">
+                                <div className="vn_inner">
+                                    <div className="vn_icon_square bck_yellow"></div>
+                                    <div
+                                        className="vn_icon"
+                                        style={{
+                                            background: `url(${icon_location})`
+                                        }}
+                                    >
+
+                                    </div>
+                                    <div className="vn_title">
+                                            Event Location
+                                    </div>
+                                    <div className="vn_desc">
+                                        Lagos State, Nigeria
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </Zoom>
 
                 </div>
            </div>
